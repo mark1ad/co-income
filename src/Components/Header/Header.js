6c1a65f28100c8897d2e2320c0448ebe2807446c
Mark De May <markd@frii.com>
2018-05-24 10:30:50 -0600

    Header menu.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
 
 const Header = () => {
   return (
-    <div></div>
+    <Navbar collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="/">Colorado Personal Income</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#/byyear">
+            By Year
+          </NavItem>
+          <NavItem eventKey={2} href="#/bycounty">
+            By County
+          </NavItem>
+          <NavItem eventKey={3} href="#/about">
+            About
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
