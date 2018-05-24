d131d7e4fab8b7b92cdcdda84f88f9db4b535615
Mark De May <markd@frii.com>
2018-05-24 10:47:53 -0600

    Set up routing. About component stub.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+
 import './App.css';
 
-import Header from "./Components/Header/Header"
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-      </div>
+      <HashRouter>
+        <div className="App">
+          <Header />
+          <Route path="/about" component={About} />
+        </div>
+      </HashRouter>
     );
   }
 }
