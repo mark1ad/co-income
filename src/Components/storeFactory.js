15ca3a43af121ef81cb7e798b895bde799a3cf10
Mark De May <markd@frii.com>
2018-06-08 09:44:11 -0600

    Log dispatch info when NODE_ENV === development

--- src/Components/storeFactory.js
+++ src/Components/storeFactory.js
@@ -8,14 +8,18 @@ let initStore = {};
 /* eslint-disable no-unused-vars */
 const logger = store => next => action  => {
   /* eslint-disable no-console */
-  // console.groupCollapsed("dispatching ", action.type)
-  // console.log('\n>>> Dispatching ', action.type);
-  // console.log('prev state ', store.getState());
-  // console.log('action ', action);
+  if (process.env.NODE_ENV === 'development') {
+    console.groupCollapsed('dispatching ', action.type);
+    console.log('\n>>> Dispatching ', action.type);
+    console.log('prev state ', store.getState());
+    console.log('action ', action);
+  }
   next(action);
-  // console.log('next state ', store.getState());
-  // console.log('\n');
-  // console.groupEnd();
+  if (process.env.NODE_ENV === 'development') {
+    console.log('next state ', store.getState());
+    console.log('\n');
+    console.groupEnd();
+  }
   /* eslint-enable no-console */
 };
 /* eslint-enable no-unused-vars */
