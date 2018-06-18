dec55ddb8e4d82e8f3845e850d4f0b11d0daf272
Mark De May <markd@frii.com>
2018-06-18 10:19:38 -0600

    If ByCounty mounts and county is not set, get data for Adams county.

--- src/Components/ByCounty/ByCounty.js
+++ src/Components/ByCounty/ByCounty.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 
 import './ByCounty.css';
@@ -9,22 +9,37 @@ import ShowCountySelectionList from
 import ShowByCountyGraph from
   './ShowByCountyGraph';
 
-const ByCounty = (props) => {
-  let name = (props.County === undefined || props.County.name === undefined)
-    ? 'Select A County' : props.County.name;
-  return (
-    <div className="by-county">
-      <ShowCountySelectionList />
-      <div className="by-county-graph">
-        <h2>{name}</h2>
-        <ShowByCountyGraph county={props.County} />
-      </div>
-    </div>
-  );
-};
+class ByCounty extends Component {
+  componentDidMount() {
+    if (this.props.getDefaultCounty !== undefined &&
+          (this.props.County === undefined || this.props.County.name === undefined) ) {
+      this.props.getDefaultCounty();
+    }
+  }
+
+  render() {
+    if (this.props.County === undefined
+        || this.props.County.name === undefined) {
+      return (
+        <div className="by-county"></div>
+      );
+    }else {
+      return (
+        <div className="by-county">
+          <ShowCountySelectionList />
+          <div className="by-county-graph">
+            <h2>{this.props.County.name}</h2>
+            <ShowByCountyGraph county={this.props.County} />
+          </div>
+        </div>
+      );
+    }
+  }
+}
 
 ByCounty.propTypes = {
-  County: PropTypes.object.isRequired
+  County: PropTypes.object.isRequired,
+  getDefaultCounty: PropTypes.func.isRequired
 };
 
 export default ByCounty;
